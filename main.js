2c42fe9c9ecbfc81c5222baeb3d195c26d9f8c17
Karla <kcherrera@uh.edu>
2025-11-17 20:54:01 -0800

    adding giant donut

--- main.js
+++ main.js
@@ -17,6 +17,7 @@ const camera = new THREE.PerspectiveCamera(
 );
 
 const renderer = new THREE.WebGLRenderer();
+renderer.setClearColor("#1B0B24");
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setAnimationLoop(animate);
 document.body.appendChild(renderer.domElement);
@@ -37,8 +38,8 @@ document.body.appendChild(renderer.domElement);
 
 const radius =  2.0;  
 const tubeRadius =  1.0;  
-const radialSegments = 24;  
-const tubularSegments =  24;  
+const radialSegments = 32;  
+const tubularSegments =  32;  
 const geometry = new THREE.TorusGeometry(
 	radius, tubeRadius,
 	radialSegments, tubularSegments );
@@ -52,11 +53,11 @@ scene.add(donut);
 
 const radius2 =  2.0;  
 const tubeRadius2 =  1.0;  
-const radialSegments2 = 24;  
-const tubularSegments2 =  24;  
+const radialSegments2 = 32;  
+const tubularSegments2 =  32;  
 const geometry2 = new THREE.TorusGeometry(
-	radius, tubeRadius,
-	radialSegments, tubularSegments );
+	radius2, tubeRadius2,
+	radialSegments2, tubularSegments2 );
 const material2 = new THREE.MeshBasicMaterial({ color: "#D583DF" });
 const donut2 = new THREE.Mesh(geometry2, material2);
 donut2.position.y = 0.5;
@@ -65,16 +66,34 @@ donut2.rotation.x = 5.0;
 // donut.scale.y = 2;
 scene.add(donut2);
 
+const radius3 =  1.5;  
+const tubeRadius3 =  0.5;  
+const radialSegments3 = 32;  
+const tubularSegments3 =  32;  
+const geometry3 = new THREE.TorusGeometry(
+	radius3, tubeRadius3,
+	radialSegments3, tubularSegments3 );
+const material3 = new THREE.MeshBasicMaterial({ color: "#2C3643" });
+const donut3 = new THREE.Mesh(geometry3, material3);
+donut3.position.y = 0.5;
+donut3.position.x = 1.0;
+donut3.rotation.x = 5.0;
+donut3.scale.set(2, 2, 2);
+scene.add(donut3);
 
 camera.position.z = 5;
 camera.position.y = 7;
 // camera.up.set(0, 0, 1);
 camera.lookAt(0, 0, 0);
 
+var frame = 0;
+
 const controls = new OrbitControls(camera, renderer.domElement)
 function animate() {
   donut.rotation.x += 0.03;
   donut2.rotation.y += 0.05;
+  donut3.scale.y += 0.005;
+  donut3.scale.x += 0.005;
   renderer.render(scene, camera);
   controls.update();
 }
